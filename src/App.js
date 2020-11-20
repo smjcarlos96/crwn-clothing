5dca51c7ea73d441df0c554033d759775c36c096
smjcarlos96 <smjcarlos96@gmail.com>
2020-11-20 19:11:27 +0100

    Adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -3,19 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';;
 
 function App() {
   return (
     <div>
-      {
-        /* 
-        * Link tag is similar to HTML 'a' tag with 'to' parameter as path to reach.
-        * Route component is a link used by the router to a page:
-        * - path will make load a route when it finds its path string in the URL.
-        * - exact is a boolean. If ommited it's true by default. It's used to load only when path is strictly the same as URL parameters.
-        * Switch component manages which Route should load according their paths. If Switch weren't used Routes would load sequentially.
-        */
-      }
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
